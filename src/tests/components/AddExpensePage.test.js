86ee02003353df1e1798802233004b41939957a7
nzanghi <nvzanghi>
2017-12-13 15:53:34 -0500

    Converted EditExpensePage into a class based component.

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -6,18 +6,17 @@ import expenses from '../fixtures/expenses';
 let onSubmit, history, wrapper;
 
 beforeEach(() => {
-	onSubmit = jest.fn();
+  onSubmit = jest.fn();
   history = { push: jest.fn() };
   wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />);
-
-})
+});
 
 test('should render AddExpensePage correctly', () => {
   expect(wrapper).toMatchSnapshot();
 });
 
 test('should handle onSubmit', () => {
-	wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
-	expect(history.push).toHaveBeenLastCalledWith('/');
-	expect(onSubmit).toHaveBeenLastCalledWith(expenses[1])
+  wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
+  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(onSubmit).toHaveBeenLastCalledWith(expenses[1]);
 });
---
+++ src/tests/components/EditExpensePage.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { EditExpensePage } from '../../components/EditExpensePage';
+import expenses from '../fixtures/expenses';
+
+let wrapper, history, editExpense, removeExpense;
+
+beforeEach(() => {
+  editExpense = jest.fn();
+  history = { push: jest.fn() };
+  removeExpense = jest.fn();
+  wrapper = shallow(
+    <EditExpensePage
+      editExpense={editExpense}
+      removeExpense={removeExpense}
+      history={history}
+      expense={expenses[2]}
+    />
+  );
+});
+
+test('should render EditExpensePage', () => {
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should handle editExpense', () => {
+  wrapper.find('ExpenseForm').prop('onSubmit')(expenses[2]);
+  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(editExpense).toHaveBeenLastCalledWith(expenses[2].id, expenses[2]);
+});
+// spies
+
+// should handle removeExpense
+// spies
