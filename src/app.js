dbba262c0706b17b0e1baa1ff4ea01bf1d675af7
nzanghi <nvzanghi>
2017-12-14 13:48:26 -0500

    Added webpack prod configuration

--- src/app.js
+++ src/app.js
@@ -13,15 +13,6 @@ import moment from 'moment';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500, createdAt: moment().subtract(1, 'week').valueOf() }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 5100, createdAt: moment().add(1, 'week').valueOf() }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500, createdAt: moment().subtract(1, 'month').valueOf() }));
-
-const state = store.getState();
-console.log(state);
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
