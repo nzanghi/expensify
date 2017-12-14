7fcd68b5b4007a70f7287c760f64dfda83816eac
nzanghi <nvzanghi>
2017-12-14 12:09:14 -0500

    Added test cases for ExpenseListFilters

--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -28,7 +28,10 @@ test('should handle editExpense', () => {
   expect(history.push).toHaveBeenLastCalledWith('/');
   expect(editExpense).toHaveBeenLastCalledWith(expenses[2].id, expenses[2]);
 });
-// spies
 
-// should handle removeExpense
+test('should handle removeExpense', () => {
+  wrapper.find('button').simulate('click');
+  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(removeExpense).toHaveBeenLastCalledWith({id: expenses[2].id});
+});
 // spies
